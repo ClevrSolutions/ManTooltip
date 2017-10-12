c14500d50ee6f1ce3cf16155e804b302e6d00cb3
McDoyen <mrpoloh@yahoo.co.uk>
2017-10-12 12:04:32 +0300

    Implement widget structure

--- src/ManTooltip/widget/ManTooltip.js
+++ src/ManTooltip/widget/ManTooltip.js
@@ -1,9 +1,9 @@
 /**
- The Help Text Viewer provides the possibility to enhance your forms by adding help buttons.
- These buttons display a help message when clicked or hovered.
+The Help Text Viewer provides the possibility to enhance your forms by adding help buttons.
+These buttons display a help message when clicked or hovered. 
 
- Optionally, the buttons can be hidden by default, with a global switch (the Help Text Trigger) to show or hide them.
- */
+Optionally, the buttons can be hidden by default, with a global switch (the Help Text Trigger) to show or hide them. 
+*/
 define([
     'dojo/_base/declare',
     'mxui/widget/_WidgetBase',
@@ -19,163 +19,164 @@ define([
     'use strict';
 
     return declare('ManTooltip.widget.ManTooltip', [ _WidgetBase ], {
-        inputargs: {
-            text : '',
-            showonhover : true,
-            width : 300,
-            height : 300,
-            closeClick : false,
-            position : 'popup'
-        },
+		
+	inputargs: {
+		text : '',
+		showonhover : true,
+		width : 300,
+		height : 300,
+		closeClick : false,
+		position : 'popup'
+	},
+	
+	//IMPLEMENTATION
+	domNode: null,
+	topic : "CustomWidget/ManTooltip",
+	imgNode : null,
+	handle : null,
+	helpNode : null,
+	helpvisible: false,
+	windowEvt : null,
+	
+  postCreate : function(){
+		logger.debug(this.id + ".postCreate");
 
-        //IMPLEMENTATION
-        domNode: null,
-        topic : "CustomWidget/ManTooltip",
-        imgNode : null,
-        handle : null,
-        helpNode : null,
-        helpvisible: false,
-        windowEvt : null,
+		//img node
+		this.imgNode = dom.create("div", {
+			'class' : 'text-info ManTooltipButton glyphicon glyphicon-info-sign'
+		});
+		this.domNode.appendChild(this.imgNode);
+		this.connect(this.imgNode, 'onclick', lang.hitch(this, this.toggleHelp, true));
+		
+		if (this.showonhover) {
+			this.connect(this.imgNode, 'onmouseenter', lang.hitch(this, this.showHelp, true, false));
+			this.connect(this.imgNode, 'onmouseleave', lang.hitch(this, this.showHelp, false, false));
+		}
+		
+		//help node
+		this.createHelp();
+		
+		//this.stateChange(this.startvisible);
+		this.handle = topic.subscribe(this.topic, this, this.stateChange);
 
-        postCreate : function(){
-            logger.debug(this.id + ".postCreate");
+		this.handle.remove();
+		
+		this.actLoaded();
+	},
 
-            //img node
-            this.imgNode = dom.create("div", {
-                'class' : 'text-info ManTooltipButton glyphicon glyphicon-info-sign'
-            });
-            this.domNode.appendChild(this.imgNode);
-            this.connect(this.imgNode, 'onclick', lang.hitch(this, this.toggleHelp, true));
-
-            if (this.showonhover) {
-                this.connect(this.imgNode, 'onmouseenter', lang.hitch(this, this.showHelp, true, false));
-                this.connect(this.imgNode, 'onmouseleave', lang.hitch(this, this.showHelp, false, false));
-            }
-
-            //help node
-            this.createHelp();
-
-            //this.stateChange(this.startvisible);
-			this.handle = topic.subscribe(this.topic, this, this.stateChange);
-			
-			this.handle.remove();
-
-            this.actLoaded();
-        },
-
-        stateChange : function(newstate) {
-            if (newstate)
-                domStyle.set(this.imgNode, "display", "block")
-        },
-
-        createHelp : function () {
-            this.helpNode = dom.create("div", {'class' : 'ManTooltipBox label label-primary'});
-            domStyle.set(this.helpNode, 'display', 'none');
-            var input = this.text.replace(/\n/g, '<br />');
-            html.set(this.helpNode, input);
-            domStyle.set(this.helpNode, {
-                'width' : this.width + 'px',
-                'maxHeight' : this.height + 'px'
-            });
-            this.connect(this.helpNode, 'onclick', lang.hitch(this, this.toggleHelp, true));
-            //document.body.appendChild(this.helpNode);
-            if (this.position == 'popup')
+	stateChange : function(newstate) {
+		if (newstate)
+		domStyle.set(this.imgNode, "display", "block")
+	},
+	
+	createHelp : function () {
+		this.helpNode = dom.create("div", {'class' : 'ManTooltipBox label label-primary'});
+		domStyle.set(this.helpNode, 'display', 'none');
+		var input = this.text.replace(/\n/g, '<br />');
+		html.set(this.helpNode, input);
+		domStyle.set(this.helpNode, {
+			'width' : this.width + 'px',
+			'maxHeight' : this.height + 'px'
+		});
+		this.connect(this.helpNode, 'onclick', lang.hitch(this, this.toggleHelp, true));
+		//document.body.appendChild(this.helpNode);
+		if (this.position == 'popup')
 			win.body().appendChild(this.helpNode);
-            else {
-                this.domNode.appendChild(this.helpNode);
-                domStyle.set(this.domNode, 'position', 'relative');
-            }
-        },
+		else {
+			this.domNode.appendChild(this.helpNode);
+			domStyle.set(this.domNode, 'position', 'relative');
+		}
+	},
 
-		toggleHelp: function (clicked, e) {
-			this.helpvisible = !this.helpvisible;
-			this.showHelp(this.helpvisible, clicked);
-			if (e) {
-				e.preventDefault();
-				e.stopPropagation();
+	toggleHelp : function(clicked, e) {
+		this.helpvisible = !this.helpvisible;
+		this.showHelp(this.helpvisible, clicked);
+		if (e) {
+			e.preventDefault();
+			e.stopPropagation();
+		}
+	},
+	
+	windowClick : function () {
+		if (this.windowEvt) {
+			this.disconnect(this.windowEvt);
+			this.windowEvt = null;
+		}
+		this.helpvisible = false;
+		domStyle.set(this.helpNode, 'display', 'none');
+	},
+	
+	showHelp : function(show, clicked) {
+		if (show || this.helpvisible) {
+			if (this.closeClick && clicked && show)
+				this.windowEvt = this.connect(document.body, 'onclick', this.windowClick);
+
+			if (this.position == 'popup') {
+				var coords = dojoHtml.coords(this.imgNode, true);
+				domStyle.set(this.helpNode, {
+					'display' : 'block',
+					'top' : (coords.y + 30)+'px',
+					'left': (window.innerWidth < coords.x + 30 + this.width ? coords.x - this.height - 30 : coords.x + 30)+'px'
+				});
 			}
-		},
+			else {
+				domStyle.set(this.helpNode, {
+					'display' : 'block',
+					'top' : '30px',
+					'left': this.position == 'right' ? '30px' : (-30 - this.width) + 'px'
+				});
+			}
 
-        windowClick : function () {
-            if (this.windowEvt) {
-                this.disconnect(this.windowEvt);
-                this.windowEvt = null;
-            }
-            this.helpvisible = false;
-            domStyle.set(this.helpNode, 'display', 'none');
-        },
+			var currentNode = this.helpNode;
+			var self = this;
 
-        showHelp : function(show, clicked) {
-            if (show || this.helpvisible) {
-                if (this.closeClick && clicked && show)
-                    this.windowEvt = this.connect(document.body, 'onclick', this.windowClick);
+			query('div[class^=mx-name-ManTooltip').forEach(function(node) {
+				var widget = dijit.registry.getEnclosingWidget(node);
+				if (widget != self) {
+					widget.helpvisible = false;
+				}
+			});
 
-                if (this.position == 'popup') {
-                    var coords = dojoHtml.coords(this.imgNode, true);
-                    domStyle.set(this.helpNode, {
-                        'display' : 'block',
-                        'top' : (coords.y + 30)+'px',
-                        'left': (window.innerWidth < coords.x + 30 + this.width ? coords.x - this.height - 30 : coords.x + 30)+'px'
-                    });
-                }
-                else {
-                    domStyle.set(this.helpNode, {
-                        'display' : 'block',
-                        'top' : '30px',
-                        'left': this.position == 'right' ? '30px' : (-30 - this.width) + 'px'
-                    });
-                }
-
-                var currentNode = this.helpNode;
-                var self = this;
-
-                query('div[class^=mx-name-ManTooltip').forEach(function(node) {
-                    var widget = dijit.registry.getEnclosingWidget(node);
-                    if (widget != self) {
-                        widget.helpvisible = false;
-                    }
-                });
-
-                query('.ManTooltipBox:style="display:block"').forEach(function(node) {
-                    if (node != currentNode) {
-                        domStyle.set(node, {
-                            'display' : 'none'
-                        });
-                    }
-                });
-            }
-            else {
-                this.helpvisible = false;
-                domStyle.set(this.helpNode, 'display', 'none');
-            }
-        },
-
-        suspended : function() {
-            if (this.windowEvt != null) {
-                this.disconnect(this.windowEvt);
-                this.windowEvt = null;
-            }
-            this.showHelp(false);
-        },
-
-        uninitialize : function() {
-
-            try {
-                if (this.windowEvt != null) {
-                    this.disconnect(this.windowEvt);
-                    this.windowEvt = null;
-                    logger.debug(this.id + ".uninitialize");
-                }
-                if (this.helpNode != null)
-                    document.body.removeChild(this.helpNode);
-                if (this.handle != null)
-				topic.unsubscribe(this.handle);
-            }
-            catch(e) {
-                logger.warn("error on helptextviewer unload: " + e);
-            }
-        }
-    });
-});
+			query('.ManTooltipBox:style="display:block"').forEach(function(node) {
+				if (node != currentNode) {
+					domStyle.set(node, {
+						'display' : 'none'
+					});
+				}
+			});
+		}
+		else {
+			this.helpvisible = false;
+			domStyle.set(this.helpNode, 'display', 'none');
+		}
+	},
+	
+	suspended : function() {
+		if (this.windowEvt != null) {
+			this.disconnect(this.windowEvt);
+			this.windowEvt = null;
+		}
+		this.showHelp(false);
+	},
+	
+	uninitialize : function() {
+		
+		try {
+			if (this.windowEvt != null) {
+				this.disconnect(this.windowEvt);
+				this.windowEvt = null;
+				logger.debug(this.id + ".uninitialize");
+			}
+			if (this.helpNode != null)
+				document.body.removeChild(this.helpNode);
+			if (this.handle != null)
+			topic.unsubscribe(this.handle);
+		}
+		catch(e) {
+			logger.warn("error on helptextviewer unload: " + e);
+		}
+	}
+	});
+	});
 
 require([ 'ManTooltip/widget/ManTooltip' ]);
